e7e2d0e2f0702092f4cf1fbccb227e424e63e71e
NrdyBhu1@gmail.com <bunnu16aug>
2021-01-20 18:48:22 +0530

    Working model ready

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn input() -> String {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
-    return guess;
+    return guess.trim().to_string();
 }
 
 fn print_header(){
@@ -15,9 +15,8 @@ fn print_header(){
 }
 
 fn to_int(num: String) -> i32{
-    let intnum: i32 = num
-        .parse()
-        .expect("Not a number!");
+    let intnum: i32 = num.parse::<i32>().unwrap();//.expect("Not a number");
+        // .expect("Not a number!");
     return intnum;
 }
 
@@ -25,21 +24,59 @@ fn get_valid_answer() -> i32 {
     let option: i32;
     option = loop {
         let inp_str = input();
-        let inp = to_int(inp_str);
+        let inp = to_int(inp_str.to_string());
 
         if inp == 1 || inp == 2{
             break inp;
         }
+        println!("That is not a valid option");
     };
 
     return option;
 }
 
+fn check_divisibility(num_one: i32, num_two: i32) -> bool {
+    if num_one % num_two == 0{
+        return true;
+    }else {
+        return false;
+    }
+}
+
+fn check_if_prime(num: i32) -> bool {
+    let mut is_prime: bool = true;
+    for j in 1..9 {
+        if j != 1 && j != num {
+            if check_divisibility(num, j){
+                is_prime = false;
+                break;
+            }else {
+                is_prime = true;
+            }
+        }
+    }
+
+    return is_prime;
+}
+
+fn get_till_a_number(number: i32){
+    for i in 1..number {
+        if check_if_prime(i) {
+            println!("{}", i);
+        } else {
+            continue;
+        }
+    }
+}
+
 fn main() {
-    let valid_ans: i32;
     print_header();
     println!("1. Print prime numbers till a number");
     println!("2. Print number of prime numbers");
-    valid_ans = get_valid_answer();
-    println!("ok: {}", valid_ans);
+    let valid_ans: i32 = get_valid_answer();
+    if valid_ans == 1 {
+        print!("Give a number");
+        let till_number: i32 = to_int(input());
+        get_till_a_number(till_number);
+    }
 }
